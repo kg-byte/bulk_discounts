e5abc6db651390aeb4cc55d73b1b675f49575b2a
Kim G <xiaole.guo@yahoo.com>
2022-04-23 11:29:08 -0600

    merchant bulk discounts page has link to discount show page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,8 @@ class BulkDiscountsController < ApplicationController
   def index
     @bulk_discounts = Merchant.find(params[:merchant_id]).bulk_discounts
   end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
 end
--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "merchants bulk discounts index page", type: :feature do
     @merchant2 = Merchant.create!(name: "Williamson Group")
   end
 
-  it 'has a link to all bulk discounts' do 
+  it 'lists bulk discounts and their attributes' do 
     bulk_discount1 = @merchant1.bulk_discounts.create!(quantity:10, discount:0.15)
     bulk_discount2 = @merchant1.bulk_discounts.create!(quantity:15, discount:0.2)
     bulk_discount3 = @merchant2.bulk_discounts.create!(quantity:20, discount:0.25)
@@ -16,6 +16,21 @@ RSpec.describe "merchants bulk discounts index page", type: :feature do
       expect(page).to have_content('Quantity threshold: 10, Percentage discount: 15%')
       expect(page).to have_content('Quantity threshold: 15, Percentage discount: 20%')
       expect(page).to_not have_content('Quantity threshold:20, Percentage discount: 25%')
+  end
+
+  it 'has a link to the show page of each bulk discount' do 
+    bulk_discount1 = @merchant1.bulk_discounts.create!(quantity:10, discount:0.15)
+    bulk_discount2 = @merchant1.bulk_discounts.create!(quantity:15, discount:0.2)
+    bulk_discount3 = @merchant2.bulk_discounts.create!(quantity:20, discount:0.25)
+    
+    visit merchant_bulk_discounts_path(@merchant1.id)
+
+    within("#bulk_discount-#{bulk_discount1.id}") do 
+    
+      click_link 'Go to This Discount'
+
+      expect(current_path).to eq bulk_discount_path(bulk_discount1.id)
+    end
   end
 
 end
