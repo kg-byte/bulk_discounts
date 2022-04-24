36b6c7d69011fa708388fd3ead788a98af8e723a
Kim G <xiaole.guo@yahoo.com>
2022-04-24 09:05:02 -0600

    refactor admin invoice show page to show percentage applied instead of link

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -18,19 +18,4 @@ RSpec.describe 'admin invoices index page' do
   end
 
 
-  it 'shows a link to the applied bulk discount if applicable and when invoice is marked completed' do 
-    merchant3 = FactoryBot.create_list(:merchant, 1)[0]
-    item5 = FactoryBot.create_list(:item, 1, merchant: merchant3)[0]
-    invoice4 = FactoryBot.create_list(:invoice, 1)[0]
-    invoice_item5 = FactoryBot.create_list(:invoice_item, 1, item: item5, invoice: invoice4, unit_price: 1000, quantity: 15)[0]
-    discount1 = merchant3.bulk_discounts.create!(quantity: 10, discount: 0.1)
-    discount2 = merchant3.bulk_discounts.create!(quantity: 15, discount: 0.2)
-
-    visit "/merchants/#{merchant3.id}/invoices/#{invoice4.id}"
-
-    within("#invoice_item-#{invoice_item5.id}") do 
-      click_link 'Applied Bulk Discount'
-      expect(current_path).to eq merchant_bulk_discount_path(merchant3.id, discount2.id)
-    end
-  end
 end
