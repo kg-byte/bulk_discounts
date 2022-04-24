9a774b40f0e0b3dd473bde2bd7d7885335bc926b
Kim G <xiaole.guo@yahoo.com>
2022-04-23 23:03:56 -0600

    add updatable method in bulk discount model-merchant unable to update/destroy discount with pending invoices

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BulkDiscount, type: :model do
 
   describe 'associations' do
 	it {should belong_to :merchant}
+  it {should have_many(:invoices).through(:merchant)}
+  # it {should have_many(:invoice_items).through(:invoices)}
   end
 
   describe 'validations' do
@@ -15,4 +17,26 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_numericality_of(:discount).is_less_than_or_equal_to(0.99) }
   end
 
+  describe 'instance methods' do 
+    describe '.updatable?' do 
+      it 'returns true when there are not pending invoices' do 
+        Invoice.destroy_all
+        merchant1 = FactoryBot.create_list(:merchant,1)[0]
+        item1 = FactoryBot.create_list(:item, 1, merchant: merchant1)[0]
+        invoice1 = FactoryBot.create_list(:invoice, 1, status: 0)[0]
+        invoice2 = FactoryBot.create_list(:invoice, 1, status: 1)[0]
+        invoice3 = FactoryBot.create_list(:invoice, 1, status: 2)[0]
+        FactoryBot.create_list(:invoice_item, 1, invoice: invoice1, item: item1, quantity: 5)
+        FactoryBot.create_list(:invoice_item, 1, invoice: invoice2, item: item1, quantity: 10)
+        FactoryBot.create_list(:invoice_item, 1, invoice: invoice3, item: item1, quantity: 20)
+        bulk_discount1 = merchant1.bulk_discounts.create(quantity: 5, discount: 0.05)
+        bulk_discount2 = merchant1.bulk_discounts.create(quantity: 10, discount: 0.1)
+        bulk_discount3 = merchant1.bulk_discounts.create(quantity: 20, discount: 0.2)
+    
+        expect(bulk_discount1.updatable?).to be true
+        expect(bulk_discount2.updatable?).to be false
+        expect(bulk_discount3.updatable?).to be true
+      end
+    end
+  end
 end
